e8e1501bd6f397eba77018502091e208011c0c00
Morgan Sell <MorganPSell@gmail.com>
2021-01-12 19:52:09 -0800

    added mse_cross_val fcn for decision trees and bagging to ch8_utils. Still need build bagging permutation importance

--- exercises/src/ch8_utils.py
+++ exercises/src/ch8_utils.py
@@ -31,3 +31,71 @@ def print_tree(estimator, features, class_names=None, filled=True):
     export_graphviz(estimator, out_file=dot_data, feature_names=features, class_names=classn, filled=filled)
     graph = pydot.graph_from_dot_data(dot_data.getvalue())
     return graph
+
+
+def calc_train_test_mse_decision_tree_regr(X_train, X_test, y_train, y_test, max_depth, cv):
+    '''
+    Fits and predicts a tree-based regression model for a range of depths.
+    Uses cross validation to calculate the mean squared error for the training set.
+    Calculates the mean squared error for the test set for each depth.
+    
+    Returns:
+        train_c_val_df (df)      : All MSEs computer for all folds for each depth.
+        avg_train_mse_arr (arr)  : The average MSE for the result of the cross validation.
+        test_mse_arr (arr)       : The test MSEs for each depth.
+    '''
+    avg_train_mse_arr = []
+    train_cross_val_df = pd.DataFrame()
+    test_mse_arr = []
+    
+    for depth in range(1, max_depth+1):
+        regr = DecisionTreeRegressor(max_depth=depth)
+        regr.fit(X_train, y_train)
+        
+        # training set metrics
+        train_cross_val_arr = -1 * cross_val_score(regr, X_train, y_train, cv=10, scoring='neg_mean_squared_error', n_jobs=-1)
+        train_cross_val_df[str(depth) + '_depth'] = train_cross_val_arr
+
+        train_mse = np.mean(train_cross_val_arr)
+        avg_train_mse_arr.append(train_mse)
+
+        # test set
+        pred = regr.predict(X_test)
+        test_mse = mean_squared_error(pred, y_test)
+        test_mse_arr.append(test_mse)
+
+    return train_cross_val_df, avg_train_mse_arr, test_mse_arr
+
+
+def calc_train_test_mse_bagging_regr(X_train, X_test, y_train, y_test, max_depth, cv):
+    '''
+    Fits and predicts a tree-based regression model for a range of depths.
+    Uses cross validation to calculate the mean squared error for the training set.
+    Calculates the mean squared error for the test set for each depth.
+    
+    Returns:
+        train_c_val_df (df)      : All MSEs computer for all folds for each depth.
+        avg_train_mse_arr (arr)  : The average MSE for the result of the cross validation.
+        test_mse_arr (arr)       : The test MSEs for each depth.
+    '''
+    avg_train_mse_arr = []
+    train_cross_val_df = pd.DataFrame()
+    test_mse_arr = []
+    
+    for depth in range(1, max_depth+1):
+        regr = RandomForestRegressor(max_depth=depth, max_features=X_train.shape[1])
+        regr.fit(X_train, y_train)
+        
+        # training set metrics
+        train_cross_val_arr = -1 * cross_val_score(regr, X_train, y_train, cv=10, scoring='neg_mean_squared_error', n_jobs=-1)
+        train_cross_val_df[str(depth) + '_depth'] = train_cross_val_arr
+
+        train_mse = np.mean(train_cross_val_arr)
+        avg_train_mse_arr.append(train_mse)
+
+        # test set
+        pred = regr.predict(X_test)
+        test_mse = mean_squared_error(pred, y_test)
+        test_mse_arr.append(test_mse)
+
+    return train_cross_val_df, avg_train_mse_arr, test_mse_arr
